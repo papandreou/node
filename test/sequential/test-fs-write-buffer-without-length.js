e029d2288e959c1d0cf73ff41b9411a0c6402844
Andreas Lind <andreas@one.com>
2016-07-23 22:43:41 +0200

    fs: Fix default params for fs.write
    Add support for fs.write(fd, buffer, cb) and fs.write(fd, buffer, offset, cb)
    as documented at
    https://nodejs.org/api/fs.html#fs_fs_write_fd_data_position_encoding_callback

---
+++ test/sequential/test-fs-write-buffer-without-length.js
@@ -0,0 +1,27 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const path = require('path');
+const Buffer = require('buffer').Buffer;
+const fs = require('fs');
+const filename = path.join(common.tmpDir, 'write.txt');
+
+common.refreshTmpDir();
+
+fs.open(filename, 'w', 0o644, common.mustCall(function(err, fd) {
+  if (err) throw err;
+
+  fs.write(fd,
+           Buffer.from('hello'),
+           3,
+           common.mustCall(function(err, written) {
+             if (err) throw err;
+
+             assert.equal(2, written);
+             fs.closeSync(fd);
+
+             var found = fs.readFileSync(filename, 'utf8');
+             assert.deepStrictEqual('lo', found);
+             fs.unlinkSync(filename);
+           }));
+}));
