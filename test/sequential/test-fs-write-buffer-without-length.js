a4265b5059c47153b83d284f9433086c8945b353
Andreas Lind <andreas@one.com>
2016-07-23 23:48:33 +0200

    var => const

--- test/sequential/test-fs-write-buffer-without-length.js
+++ test/sequential/test-fs-write-buffer-without-length.js
@@ -11,13 +11,13 @@ common.refreshTmpDir();
 fs.open(filename, 'w', 0o644, common.mustCall(function(err, fd) {
   if (err) throw err;
 
-  var cb = common.mustCall(function(err, written) {
+  const cb = common.mustCall(function(err, written) {
     if (err) throw err;
 
     assert.equal(2, written);
     fs.closeSync(fd);
 
-    var found = fs.readFileSync(filename, 'utf8');
+    const found = fs.readFileSync(filename, 'utf8');
     assert.deepStrictEqual('lo', found);
     fs.unlinkSync(filename);
   });
