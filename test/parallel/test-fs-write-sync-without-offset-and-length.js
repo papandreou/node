d60908cee91927035da6bbee4baa7b39f860c91d
Andreas Lind <andreas@one.com>
2016-08-01 03:10:17 +0200

    Fix fs.writeSync as well (see #7879).

---
+++ test/parallel/test-fs-write-sync-without-offset-and-length.js
@@ -0,0 +1,23 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
+const fn = path.join(common.tmpDir, 'write.txt');
+
+common.refreshTmpDir();
+
+const foo = 'foo';
+const fd = fs.openSync(fn, 'w');
+
+const written = fs.writeSync(fd, '');
+assert.strictEqual(0, written);
+
+fs.writeSync(fd, foo);
+
+const bar = 'bár';
+const written2 = fs.writeSync(fd, Buffer.from(bar));
+assert.ok(written2 > 3);
+fs.closeSync(fd);
+
+assert.equal(fs.readFileSync(fn), 'foobár');
